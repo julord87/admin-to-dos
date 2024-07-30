bfa92859bd876e429928852720647d16a9ed17fd
Julian Martinez Gomez <julianmzg@gmail.com>
2024-07-30 18:11:45 -0300

    feat: Add lazy loading for images in CookiesPage

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,5 +1,5 @@
 import { TabBar } from "@/components/TabBar";
-
+import { cookies } from "next/headers";
 
 export const metadata = {
  title: 'Cookies Page',
@@ -7,12 +7,17 @@ export const metadata = {
 };
 
 export default function CookiesPage() {
+
+    const cookieStorre = cookies();
+    const cookieTab = cookieStorre.get('currentTab')?.value ?? '1';
+    
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
 
         <div className="flex flex-col">
             <span className="text-3xl mb-3">Tabs</span>  
-            <TabBar /> 
+            <TabBar currentTab={Number(cookieTab)}/> 
         </div>
    
 
