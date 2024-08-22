d9553db76575ceaad11a1b1dd745df85290c6f39
Julian Martinez Gomez <julianmzg@gmail.com>
2024-08-22 09:57:12 -0300

    chore: Update npm dependencies for NextAuth.js integration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { signInEmailPassword } from "../actions/auth-actions";
 
 export const authOptions: NextAuthOptions = {
     
@@ -15,6 +17,27 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "Email" },
+                password: {  label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = await signInEmailPassword(credentials?.email ?? '', credentials?.password ?? '')
+          
+                if (user) {
+                  // Any object returned will be saved in `user` property of the JWT
+                  return user
+                } else {
+                  // If you return null then an error will be displayed advising the user to check their details.
+                  return null
+          
+                  // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            } 
         }),
     ],
 
